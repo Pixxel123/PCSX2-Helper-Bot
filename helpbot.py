48fd449659bd2be5b2fc33a8101ffb09e16df0b7
Mark Araujo <maraujo123@hotmail.co.uk>
2020-06-10 17:41:53 +0100

    Helpbot and main file rename

---
+++ helpbot.py
@@ -0,0 +1,4 @@
+# TODO: User will type in 'HelperBot! <command>'
+# 'support' command shows how to ask effective question
+# 'specs' shows how to find CPU specs
+# 'commands' shows available bot commands
