2adaebc3922127d47f545233641f7230493461c3
Mark Araujo <maraujo123@hotmail.co.uk>
2020-07-03 15:23:00 +0100

    fix: added newlines in helperbot messages

--- modules/helperbot.py
+++ modules/helperbot.py
@@ -16,11 +16,11 @@ class Helperbot():
         bot_reply = ''
         command = command.lower().strip()
         if command == 'support':
-            bot_reply = "### **Getting Help on r/PCSX2**\n\n1) Please have the following information ready:\n\n- What PCSX2 version you are using. 'Latest' is not helpful, please look at the actual version number (Found at the top of your PCSX2 window if you're not in fullscreen).\n- What CPU and GPU your PC has. The `HelperBot! specs` command will show you where you can find these on Windows 10.\n- What specific game(s) you are having problems with.\n\n2) Ask your question! Don't worry about asking  (as long as your question falls within the rules)."
+            bot_reply = "\n\n### **Getting Help on r/PCSX2**\n\n1) Please have the following information ready:\n\n- What PCSX2 version you are using. 'Latest' is not helpful, please look at the actual version number (Found at the top of your PCSX2 window if you're not in fullscreen).\n- What CPU and GPU your PC has. The `HelperBot! specs` command will show you where you can find these on Windows 10.\n- What specific game(s) you are having problems with.\n\n2) Ask your question! Don't worry about asking  (as long as your question falls within the rules)."
         if command == 'specs':
-            bot_reply = f"### **Finding PC specs on Windows 10:**\n\nOpen Task Manager by pressing 'Ctrl + Shift + Esc', then follow these screenshots to locate your CPU and GPU: [SPECS SCREENSHOT]({self.windows_specs_image})"
+            bot_reply = f"\n\n### **Finding PC specs on Windows 10:**\n\nOpen Task Manager by pressing 'Ctrl + Shift + Esc', then follow these screenshots to locate your CPU and GPU: [SPECS SCREENSHOT]({self.windows_specs_image})"
         if command == 'steam':
-            bot_reply = f"### **A guide to using your DS4 and other controllers via Steam:**\n\nA basic guide to setting up your controller:\n\n{self.steam_controller_tutorial}"
+            bot_reply = f"\n\n### **A guide to using your DS4 and other controllers via Steam:**\n\nA basic guide to setting up your controller:\n\n{self.steam_controller_tutorial}"
         if command == 'commands':
             bot_reply = "I respond to the following commands (**NOTE:** The bot call is not case-sensitive):"
             bot_reply += "\n\n`GPUBot! gpu model` - Find graphics card G3D mark and how it performs with PCSX2"
